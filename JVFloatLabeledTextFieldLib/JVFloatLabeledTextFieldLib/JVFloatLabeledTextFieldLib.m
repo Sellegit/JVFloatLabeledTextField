2662797c6654cd83dd8a48acea0e8dad5e29138e
thisiscam <Cam@Cambridges-MacBook-Air.local>
2014-08-25 23:18:36 +0800

    add transition animation

---
+++ JVFloatLabeledTextFieldLib/JVFloatLabeledTextFieldLib/JVFloatLabeledTextFieldLib.m
@@ -0,0 +1,13 @@
+//
+//  JVFloatLabeledTextFieldLib.m
+//  JVFloatLabeledTextFieldLib
+//
+//  Created by Cambridge Yang on 8/25/14.
+//  Copyright (c) 2014 Cambridge Yang. All rights reserved.
+//
+
+#import "JVFloatLabeledTextFieldLib.h"
+
+@implementation JVFloatLabeledTextFieldLib
+
+@end
